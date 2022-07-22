dafa861c5a595ece3340fa46489e2817901c8963
bradenkwebb <bkwebb23@gmail.com>
2022-07-22 13:39:59 -0600

    document and simplify code

--- src/main/java/services/PersonService.java
+++ src/main/java/services/PersonService.java
@@ -26,6 +26,15 @@ public class PersonService implements Service {
      */
     public PersonService() {}
 
+    /**
+     * Collects the requested people relevant to the user (either all, 1, or none) and returns them in an appropriate
+     * Result object.
+     *
+     * @param username the user whose family members we're searching for
+     * @param urlPath the url path determining whether we're searching for one or multiple people
+     * @return Returns either a PersonResult(), PeopleResult(), or Result() object corresponding to whether the provided
+     *          urlPath requests an individual or all people in the user's family tree and whether the
+     */
     public Result getResult(String username, String urlPath) {
         logger.entering("PersonService", "getResult");
         Result result = new PersonResult();
@@ -34,8 +43,7 @@ public class PersonService implements Service {
 
         try (Connection c = db.getConnection()) {
             conn = c;
-            PersonDAO personDAO = new PersonDAO(conn);
-            List<Person> people = personDAO.getAllFamily(username);
+            List<Person> people = new PersonDAO(conn).getAllFamily(username);
             if (isSpecific(urlPath)) {
                 result = individual(result, urlPath, people);
             } else {
@@ -64,6 +72,13 @@ public class PersonService implements Service {
         return parts.length == ID_INDEX + 1;
     }
 
+    /**
+     * Gets the Person object with matching personID from the provided list of Person objects.
+     *
+     * @param personID the ID to search for
+     * @param people the list of Person objects
+     * @return the Person() if found, otherwise null
+     */
     private Person getPersonFromList(String personID, List<Person> people) {
         for (Person person : people) {
             if (personID.equals(person.getPersonID())) {
@@ -73,11 +88,15 @@ public class PersonService implements Service {
         return null;
     }
 
-    private String getIDFromPath(String urlPath) {
-        String[] parts = urlPath.split("/");
-        return parts[ID_INDEX];
-    }
-
+    /**
+     * Determines whether person requested by the provided urlPath is in the list of people, and returns a
+     * corresponding Result object.
+     *
+     * @param r the result reference to update
+     * @param urlPath a String that should specify an individual personID
+     * @param people a list of all the people in the user's family tree
+     * @return a PersonResult() object if the personID is found in the list, otherwise, a failed Result() object
+     */
     private Result individual(Result r, String urlPath, List<Person> people) {
         Person person = getPersonFromList(getIDFromPath(urlPath), people);
         if (person != null) {
@@ -88,5 +107,16 @@ public class PersonService implements Service {
             r.setMessage("Error: Person not found (at least not in this user's tree)");
         }
         return r;
+    }
+
+    /**
+     * Obtains a personID from the provided urlPath.
+     *
+     * @param urlPath the String from which to extract the personID
+     * @return the personID as a String
+     */
+    private String getIDFromPath(String urlPath) {
+        String[] parts = urlPath.split("/");
+        return parts[ID_INDEX];
     }
 }
