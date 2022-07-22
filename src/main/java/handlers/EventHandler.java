bcbed79d3c75704974db160892dc6410c36fe5f3
bradenkwebb <bkwebb23@gmail.com>
2022-07-22 12:30:23 -0600

    simplify EventHandler

--- src/main/java/handlers/EventHandler.java
+++ src/main/java/handlers/EventHandler.java
@@ -9,7 +9,6 @@ import services.EventService;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -26,45 +25,46 @@ public class EventHandler implements Handler {
      * @param exchange the exchange containing the request from the client and used to send the response
      */
     @Override
-    public void handle(HttpExchange exchange) {
+    public void handle(HttpExchange exchange) throws IOException {
         logger.entering("EventHandler", "handle");
 
-        // Assume failure unless shown otherwise
-        boolean success = false;
         int statusCode = HttpURLConnection.HTTP_BAD_REQUEST;
         Result result = new Result();
 
-        try {
-            if (exchange.getRequestMethod().equalsIgnoreCase("get")) {
-                Headers reqHeaders = exchange.getRequestHeaders();
-                if (reqHeaders.containsKey("Authorization")) {
-                    AuthorizeService aService = new AuthorizeService();
-                    User user = aService.authorize(reqHeaders.getFirst("Authorization"));
-                    if (user != null) {
-                        EventService service = new EventService();
-                        result = service.getResult(user.getUsername(), exchange.getRequestURI().toString());
+        if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
+            statusCode = HttpURLConnection.HTTP_BAD_METHOD;
+            result.setMessage("Error: Invalid method");
+        } else {
+            Headers reqHeaders = exchange.getRequestHeaders();
+            User user = authorizeUser(reqHeaders);
 
-                        success = result.isSuccess();
-                        statusCode = (success) ? HttpURLConnection.HTTP_OK : HttpURLConnection.HTTP_BAD_REQUEST;
-                    } else {
-                        result.setMessage("Error: Invalid auth token");
-                    }
-                } else {
-                    result.setMessage("Error: Invalid auth token");
-                }
+            if (user != null) {
+                EventService service = new EventService();
+                result = service.getResult(user.getUsername(), exchange.getRequestURI().toString());
+
+                statusCode = (result.isSuccess()) ? HttpURLConnection.HTTP_OK : HttpURLConnection.HTTP_BAD_REQUEST;
             } else {
-                statusCode = HttpURLConnection.HTTP_BAD_METHOD;
-                result.setMessage("Error: Invalid method");
+                result.setMessage("Error: Invalid auth token");
             }
-
-            result.setSuccess(success);
-
-            exchange.sendResponseHeaders(statusCode, 0);
-            writeString(serialize(result), exchange.getResponseBody());
-            exchange.getResponseBody().close();
-        } catch (IOException ex) {
-            logger.log(Level.SEVERE, ex.getMessage(), ex);
-            throw new RuntimeException(ex.getMessage(), ex);
         }
+
+        exchange.sendResponseHeaders(statusCode, 0);
+        writeString(serialize(result), exchange.getResponseBody());
+        exchange.getResponseBody().close();
+    }
+
+    /**
+     * Determines whether or not a user is authorized and if so, generates their User object.
+     *
+     * @param reqHeaders The HTTP request headers
+     * @return null if the provided HTTP request holders do not contain a valid auth token, otherwise, generates
+     *      a User object corresponding to the valid authtoken.
+     */
+    private User authorizeUser(Headers reqHeaders) {
+        if (reqHeaders.containsKey("Authorization")) {
+            AuthorizeService aService = new AuthorizeService();
+            return aService.authorize(reqHeaders.getFirst("Authorization"));
+        }
+        return null;
     }
 }
