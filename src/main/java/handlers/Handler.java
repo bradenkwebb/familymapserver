9c4bd6fd50c3bb71ff329c928b24590ac7e44b27
bradenkwebb <bkwebb23@gmail.com>
2022-07-22 12:34:30 -0600

    document further

--- src/main/java/handlers/Handler.java
+++ src/main/java/handlers/Handler.java
@@ -10,16 +10,38 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public interface Handler extends HttpHandler {
+    /**
+     * Writes a String to an OutputStream.
+     *
+     * @param str the String to write
+     * @param os the destination OutputStream
+     * @throws IOException if an I/O error occurs
+     */
     default void writeString(String str, OutputStream os) throws IOException {
         OutputStreamWriter sw = new OutputStreamWriter(os);
         sw.write(str);
         sw.flush();
     }
+
+    /**
+     * Turns a Result() object into a JSON-formatted string.
+     *
+     * @param result the object to serialize
+     * @return the JSON string
+     */
     default String serialize(Result result) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         return gson.toJson(result);
     }
 
+    /**
+     * Turns an InputStream of JSON-text into an object of the provided class.
+     *
+     * @param bodyStream an InputStream, which should be in JSON-format
+     * @param classType the type of object to create (should inherit from Request)
+     * @return the resulting Object type
+     * @throws IOException if an I/O error occurs
+     */
     default Request deserialize(InputStream bodyStream, Class<? extends Request> classType) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(bodyStream, StandardCharsets.UTF_8));
         Gson gson = new Gson();
