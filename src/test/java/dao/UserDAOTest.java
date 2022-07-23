90e81b79ba33eb2830003f998278bb1a8e35aa9e
bradenkwebb <bkwebb23@gmail.com>
2022-07-22 18:33:27 -0600

    write out test cases for UserDAOTest

--- src/test/java/dao/UserDAOTest.java
+++ src/test/java/dao/UserDAOTest.java
@@ -1,9 +1,6 @@
 package dao;
 
-import dao.DataAccessException;
-import dao.Database;
-import dao.UserDAO;
-import model.Person;
+import model.AuthToken;
 import model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,7 +12,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UserDAOTest {
     private Database db;
-    private User bestUser;
+    private User braden;
+    private User anna;
     private UserDAO uDao;
 
     @BeforeEach
@@ -23,8 +21,10 @@ public class UserDAOTest {
         System.out.println("setUp() called.");
         db = new Database();
 
-        bestUser = new User("bkwebb23", "mypass", "bw@gmail.com",
+        braden = new User("bkwebb23", "mypass", "bw@gmail.com",
                             "Braden", "Webb", "m", "123");
+        anna = new User("aceverett", "password", "ae@gmail.com",
+                        "Anna", "Everett", "f", "321");
 
         Connection conn = db.getConnection();
         uDao = new UserDAO(conn);
@@ -42,31 +42,31 @@ public class UserDAOTest {
     public void insertPass() throws DataAccessException {
         System.out.println("insertPass() called.");
 
-        uDao.insert(bestUser);
-        User compareUser = uDao.find(bestUser.getUsername());
+        uDao.insert(braden);
+        User compareUser = uDao.find(braden.getUsername());
 
         assertNotNull(compareUser);
-        assertEquals(bestUser, compareUser);
+        assertEquals(braden, compareUser);
     }
 
     @Test
     public void insertFail() throws DataAccessException {
         System.out.println("insertFail() called.");
 
-        uDao.insert(bestUser);
-        assertThrows(DataAccessException.class, () -> uDao.insert(bestUser));
+        uDao.insert(braden);
+        assertThrows(DataAccessException.class, () -> uDao.insert(braden));
     }
 
     @Test
     public void findPass() throws DataAccessException {
         System.out.println("findPass() called.");
 
-        uDao.insert(bestUser);
+        uDao.insert(braden);
 
-        User compareUser = uDao.find(bestUser.getUsername());
+        User compareUser = uDao.find(braden.getUsername());
 
         assertNotNull(compareUser);
-        assertEquals(bestUser, compareUser);
+        assertEquals(braden, compareUser);
     }
 
     @Test
@@ -77,23 +77,51 @@ public class UserDAOTest {
     }
 
     @Test
-    public void updatePass() throws DataAccessException {}
+    public void updatePass() throws DataAccessException {
+        uDao.insert(braden);
+        uDao.insert(anna);
+        assertEquals(braden, uDao.find("bkwebb23"));
+        assertDoesNotThrow(() -> uDao.update(braden));
+        uDao.update(braden);
+        assertEquals(braden, uDao.find("bkwebb23"));
+        braden.setPassword("new_password");
+        assertNotEquals(braden, uDao.find("bkwebb23"));
+        uDao.update(braden);
+        braden.setPassword("old_password");
+        assertNotEquals(braden, uDao.find("bkwebb23"));
+    }
 
     @Test
-    public void updateFail() throws DataAccessException {}
+    public void updateFail() throws DataAccessException {
+        uDao.insert(anna);
+        uDao.insert(braden);
+        braden.setUsername(anna.getUsername());
+        assertThrows(DataAccessException.class, () -> uDao.update(braden));
+    }
 
     @Test
-    public void validatePass() throws DataAccessException {}
+    public void validatePass() throws DataAccessException {
+        assertDoesNotThrow(() -> uDao.validate("bkwebb23", "pass"));
+        assertFalse(uDao.validate(braden.getUsername(), braden.getPassword()));
+        uDao.insert(braden);
+        assertTrue(uDao.validate(braden.getUsername(), braden.getPassword()));
+        assertFalse(uDao.validate(anna.getUsername(), anna.getPassword()));
+    }
 
     @Test
-    public void validateFail() throws DataAccessException {}
+    public void validateFail() throws DataAccessException {
+        uDao.insert(braden);
+        assertFalse(uDao.validate(braden.getUsername(), "pass"));
+        assertFalse(uDao.validate(anna.getUsername(), anna.getPassword()));
+        assertFalse(uDao.validate(anna.getUsername(), braden.getPassword()));
+    }
 
     @Test
     public void clearPass() throws DataAccessException {
         System.out.println("clearPass() called.");
 
-        uDao.insert(bestUser);
+        uDao.insert(braden);
         uDao.clear();
-        assertDoesNotThrow(() -> uDao.insert(bestUser));
+        assertDoesNotThrow(() -> uDao.insert(braden));
     }
 }
